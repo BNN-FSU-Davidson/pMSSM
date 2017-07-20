6dfdbe1cddde47aae75e0afc6e22db5735e43197
karbo <alexanderkarbo@gmail.com>
2017-07-20 10:34:40 -0400

    One set of files to write incrementally and one to write in a batch

--- datagroup.py
+++ datagroup.py
@@ -4,36 +4,38 @@ import sys
 import linecache
 
 def main():
-    name = sys.argv[1] #name of datafile to be created
-    num = int(sys.argv[2]) #number of points to read
+    name = sys.argv[1] #name of datafile to be used
+    num = int(sys.argv[2]) #which file to read
 
-    outfile = open(str(name), 'w') #create datafile
-
-    #get input names from a sus file and write to datafile
-    for j in range(24):
-        if not (j == 10 or j == 13 or j == 16 or j == 19 or j == 22):
-            row = linecache.getline('./susyhit/sus1.dat', j+64)
-            outfile.write("{0:>16}".format(row.split()[3]))
+    if (num == 1):
+        outfile = open(str(name), 'w') #create datafile
+        #get input names from a sus file and write to datafile
+        for j in range(24):
+            if not (j == 10 or j == 13 or j == 16 or j == 19 or j == 22):
+                row = linecache.getline('./susyhit/sus'+str(num)+'.dat', j+64)
+                outfile.write("{0:>16}".format(row.split()[3]))
     
-    #get target names from a pro file and write to datafile
-    row = linecache.getline('./prospino/pro1.dat', 3)
-    outfile.write("{0:>14}".format(row.split()[13]))
-    outfile.write("{0:>14}".format(row.split()[14]))
-    outfile.write("\n")
+        #get target names from a pro file and write to datafile
+        row = linecache.getline('./prospino/pro'+str(num)+'.dat', 3)
+        outfile.write("{0:>14}".format(row.split()[13]))
+        outfile.write("{0:>14}".format(row.split()[14]))
+        outfile.write("\n")
+    else:
+        outfile = open(str(name), 'a') #open datafile
 
-    #cycle through specified number of  sus and pro files
-    for i in range(num):
-	row = linecache.getline('./prospino/pro'+str(i+1)+'.dat',1)
-	if not (float(row.split()[14]) == float(0.610)):
-            #read and write the sus data
-            for j in range(24):
-                if not (j == 10 or j == 13 or j == 16 or j == 19 or j == 22):
-                    row = linecache.getline('./susyhit/sus'+str(i+1)+'.dat', j+64)
-                    outfile.write("{0:>16}".format(row.split()[1]))
-            #read and write the pro data
-            row = linecache.getline('./prospino/pro'+str(i+1)+'.dat', 1) 
-            outfile.write("{0:>14}".format(row.split()[14]))
-            outfile.write("{0:>14}".format(row.split()[15]))
-            outfile.write("\n")
+
+    row = linecache.getline('./prospino/pro'+str(num)+'.dat',1)
+    if not (float(row.split()[14]) == float(0.610)):
+        #read and write the sus data
+        for j in range(24):
+            if not (j == 10 or j == 13 or j == 16 or j == 19 or j == 22):
+                row = linecache.getline('./susyhit/sus'+str(num)+'.dat', j+64)
+                outfile.write("{0:>16}".format(row.split()[1]))
+            
+        #read and write the pro data
+        row = linecache.getline('./prospino/pro'+str(num)+'.dat', 1) 
+        outfile.write("{0:>14}".format(row.split()[14]))
+        outfile.write("{0:>14}".format(row.split()[15]))
+        outfile.write("\n")
         
 main()
